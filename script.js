bf0397177a825c4c4a9cebb3c3886520d892b306
guilhermezardo <guilhermezardo0@gmail.com>
2023-07-24 21:26:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function enviarNumero() {
+    var numero = document.getElementById('numero').value;
+    // Aqui, você pode realizar qualquer ação que desejar com o número inserido
+    // Por exemplo, exibir o número em um alert:
+    alert("O número digitado foi: " + numero);
+}
