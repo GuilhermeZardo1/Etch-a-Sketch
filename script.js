14e571ea33402e408f27664553e6e30a77dc3312
guilhermezardo <guilhermezardo0@gmail.com>
2023-07-25 20:58:12 -0300

    final

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let numero=16;
 let container = document.querySelector('.container');
 
+
 function enviarNumero() {
-    
-    // Aqui, você pode realizar qualquer ação que desejar com o número inserido
-    // Por exemplo, exibir o número em um alert:
-    alert("O número digitado foi: " + numero);
+
+    var value = document.getElementById("selection").value;
+    numero=value;
+    container.innerHTML = "";
+    divGrid(numero);
     
 }
 
@@ -17,7 +19,7 @@ function divGrid(numero){
         row.classList.add('row');
         for (let i = 0; i <= numero; i++) {
             let cell = document.createElement('div');
-            cell.classList.add('column');
+            cell.classList.add('cell');
             
             cell.addEventListener('mouseenter', function(e){
                 e.target.style.backgroundColor = "black";
@@ -30,6 +32,6 @@ function divGrid(numero){
 document.addEventListener("DOMContentLoaded", function() {
     divGrid(numero);
 });
-console.log('hello');
+
 
  
